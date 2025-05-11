f09b6becad6cda4e2944b9f78e0db82f068ff03e
Ari Tri Wibawa <aritri772@gmail.com>
2025-05-11 17:58:44 +0700

    tambah function insertionSort

--- bubbleSort-5246.cpp
+++ bubbleSort-5246.cpp
@@ -15,6 +15,18 @@ void bubbleSort(int n, int arr[]){
 	}
 }
 
+void insertionSort(int arr[], int n){
+	for (int i=1; i<n; i++){
+		int key = arr[i];
+		int j = i-1;
+		while (key < arr[j] && j>= 0){
+		arr[j+1] = arr[j];
+		--j;	
+		}
+		arr[j+1] = key;
+	}
+}
+
 void input(int arr[], int n){
 	for (int i =0; i<n; i++){
 		cout<<"Angka ke-"<<i+1<<" :";
